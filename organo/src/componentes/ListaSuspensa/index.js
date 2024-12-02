00ecd02d7ffb33e9d34efe5881472c628c8fde7c
Wesley Borges <djwesleyborges@gmail.com>
2024-12-02 20:15:19 -0300

    versionando projeto organo

---
+++ organo/src/componentes/ListaSuspensa/index.js
@@ -0,0 +1,18 @@
+import './ListaSuspensa.css'
+
+const ListaSuspensa = (props) => {
+
+
+
+    return (
+        <div className="lista-suspensa">
+            <label>{props.label}</label>
+            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.obrigatorio} value={props.valor}>
+                <option value="">Selecione uma opção</option>
+                {props.itens.map(item => <option key={item}>{item}</option>)}
+            </select>
+        </div>
+    )
+}
+
+export default ListaSuspensa
